06eb904893e01dbc37b251de6a708f744f88d1e9
MOURAD ELGOURARI <muradelgourarion15@gmail.com>
2026-01-06 14:32:54 +0100

    Create Client Table

--- snifly/src/app/app.routes.ts
+++ snifly/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Client } from './client/client';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'client', component: Client },
+];
